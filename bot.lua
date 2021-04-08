ac5495e60e0e6203161a5b0772ddadbc5f62a6f2
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2021-04-08 10:19:17 -0400

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -1,4 +1,5 @@
 local sockURL = "ws://localhost:8080/ws"
+local prefix = "+"
 
 local s,err = http.websocket(sockURL)
 if not s then error(err) end
@@ -6,6 +7,6 @@ if not s then error(err) end
 while true do
   local json = s.receive()
   local data = textutils.unserializeJSON(json)
-  local message = data.message:gsub(">>","")
+  local message = data.message:gsub(prefix,"")
   -- Insert your bot code here.
 end
